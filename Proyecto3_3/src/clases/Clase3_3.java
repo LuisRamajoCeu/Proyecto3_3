91ab75affcbc21e119d309b4f7413cfb2779a167
LuisRamajoCeu <lramajo2587@ceuandalucia.es>
2024-10-25 16:36:53 +0200

    a

--- Proyecto3_3/src/clases/Clase3_3.java
+++ Proyecto3_3/src/clases/Clase3_3.java
@@ -3,7 +3,7 @@ package clases;
 public class Clase3_3 {
 
 	public static void main(String[] args) {
-		System.out.println("Práctica 3_3 Luis Ramajo");
+		System.out.println("Canela En Rama");
 	}
 
 }
